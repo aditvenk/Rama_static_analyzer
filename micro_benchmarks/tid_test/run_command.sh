9648f1b99555942f9bcd47dffca738d82296f4cf
Naveen Neelakandan <nave.mvn@gmail.com>
2015-11-14 17:32:26 -0600

    Simple test to ensure tid passed into thread function

---
+++ micro_benchmarks/tid_test/run_command.sh
@@ -0,0 +1,9 @@
+# first perform mem2reg
+opt -mem2reg < simple_tester.bc > simple_tester.optbc
+llvm-dis simple_tester.optbc 
+# run inline anlysis on optbc
+opt -load /scratch/neelakandan/tools/llvm-3.8-build/lib/LLVMInlineAnalysis.so -rama_inline -always-inline < simple_tester.optbc > simple_tester.optbc_inline  2> out.txt
+llvm-dis simple_tester.optbc_inline 
+# run Rama on optbc
+#opt -load /scratch/neelakandan/tools/llvm-3.8-build/lib/LLVMPointerAnalysis.so -rama < simple_tester.optbc_inline > simple_tester.optbc2
+#llvm-dis simple_tester.optbc2
